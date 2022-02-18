f40d396eb5992d7eba8eb2ebe8714b217608bf2d
ElissonRodrigues <elissonrodrigues98@gmail.com>
2022-02-18 19:33:34 -0300

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,1 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
 
+rm -rf /data/data/com.termux/files/usr/bin/heroku
+rm -rf data/data/com.termux/files/usr/lib/heroku
+
+pkg update
+pkg install nodejs -y
+npm install -g npm@latest
+npm o -g heroku
+heroku --version
